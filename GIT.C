77c4518f9f6efd583f19ed57a6c3f8485bbec32c
PrasadRanjanSikdar2000 <prasadranjan2000@gmail.com>
2019-12-05 22:52:01 +0530

    My first program upload for testing git

---
+++ GIT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char s[200];
+	clrscr();
+	printf("\t\t-----------Testing GitHub------------");
+	printf("\n\tEnter a sentence:");
+	gets(s);
+	printf("\n\tThe sentence is:\n\t\t%s",s);
+	printf("\n\t\t\t\t++++THE END++++");
+	getch();
+}
